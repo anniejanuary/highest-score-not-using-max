94245b5a6c818f6a2c2badaaf376a11afd3c9ec1
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-08-12 15:04:27 +0200

    Update highest-score-not-using-max.py

--- highest-score-not-using-max.py
+++ highest-score-not-using-max.py
@@ -3,9 +3,9 @@ for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 print(student_scores)
 
-max_val = student_scores[0]
+max_val = 0
 
-for x in student_scores[1:]:
+for x in student_scores:
     if x > max_val:
         max_val = x
-print("The highest score in the class is: ", max_val)
+print("The highest score in the class is:", max_val)
